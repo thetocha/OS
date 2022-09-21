9f7f7a0dbd85491dd5e044ca41c565042d772177
thetocha <antongub3@gmail.com>
2022-09-21 21:15:34 +0300

    ready libs on linux

---
+++ Libs/Vector/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Vector.h"
+
+int main() {
+    Vector a = Vector(2,3);
+    std::cout << a.x.value << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
