927320e0db5c2018f4e334a6c6e67731951fb145
thetocha <antongub3@gmail.com>
2022-09-28 15:54:47 +0300

    some chages in script

--- lib_with_BashScript/script.sh
+++ lib_with_BashScript/script.sh
@@ -1,5 +1,5 @@
 g++ -c -o Number/Number.o Number/Number.cpp
-ar rvs Number/libNumber.a Number/Number.o
+ar r Number/libNumber.a Number/Number.o
 g++ -c -o Vector/Vector.o Vector/Vector.cpp -INumber -LNumber -lNumber -fPIC
 g++ -shared -o Vector/libVector.so Vector/Vector.o -fPIC
 g++ -o main main.cpp -INumber -IVector ./Number/libNumber.a ./Vector/libVector.so
