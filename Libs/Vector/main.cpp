4404ebd5d17d9b2e885ab94cba2fc8ea837eb7a3
Anton Hubko <89971901+thetocha@users.noreply.github.com>
2022-11-24 18:54:13 +0300

    Delete Libs directory

--- Libs/Vector/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Vector.h"
-
-int main() {
-    Vector a = Vector(2,3);
-    std::cout << a.x.value << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
