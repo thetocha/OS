61e0a7f06760c496f10b6c7358d2c80c32cea268
Anton Hubko <89971901+thetocha@users.noreply.github.com>
2022-11-24 18:55:13 +0300

    Delete lib_with_BashScript directory

--- lib_with_BashScript/script.sh
+++
@@ -1,6 +0,0 @@
-g++ -c -o Number/Number.o Number/Number.cpp
-ar r Number/libNumber.a Number/Number.o
-g++ -c -o Vector/Vector.o Vector/Vector.cpp -INumber -LNumber -lNumber -fPIC
-g++ -shared -o Vector/libVector.so Vector/Vector.o -fPIC
-g++ -o main main.cpp -INumber -IVector Number/libNumber.a Vector/libVector.so
-./main
